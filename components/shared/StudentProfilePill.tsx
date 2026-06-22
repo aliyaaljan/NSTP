64d6021f669314c97dc559ba401b2c2a2f3c7a28
jaerishr <jaerishrabang27@gmail.com>
2026-06-22 16:14:35 +0800

    feat(studentdb): Sidebar, Profile Pill, and Hours Analytics

---
+++ components/shared/StudentProfilePill.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import Link from "next/link"
+
+
+const C = {
+  maroonDark:"#6B0D10",
+  gold:"#C8A84B",
+  goldText:"#4A2C00",
+}
+
+
+interface ProfilePillProps {
+  name:string
+  initials:string
+  section:string
+}
+
+
+export default function ProfilePill({
+  name,
+  initials,
+  section
+}:ProfilePillProps){
+
+return (
+
+<Link
+href="/student/profile"
+style={{
+display:"flex",
+alignItems:"center",
+gap:10,
+background:C.maroonDark,
+borderRadius:40,
+padding:"7px 18px 7px 8px",
+textDecoration:"none"
+}}
+>
+
+
+<div
+style={{
+width:36,
+height:36,
+borderRadius:"50%",
+background:C.gold,
+display:"flex",
+alignItems:"center",
+justifyContent:"center",
+fontSize:11,
+fontWeight:700,
+color:C.goldText
+}}
+>
+{initials}
+</div>
+
+
+<div>
+
+<div
+style={{
+color:"#fff",
+fontSize:13,
+fontWeight:700
+}}
+>
+{name}
+</div>
+
+
+<div
+style={{
+color:"rgba(255,255,255,.7)",
+fontSize:11
+}}
+>
+NSTP - {section}
+</div>
+
+
+</div>
+
+
+</Link>
+
+)
+
+}
